d5e69d4d7230eeb8778e3efc928f9720a607450c
WDoyle21 <57500204+WDoyle21@users.noreply.github.com>
2020-03-30 14:08:15 -0400

    3/30

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,14 @@
-console.log("Connected")
+//First Problem//
+let light = "Edis";
+if (light === "green"){
+    console.log("Go");
+} else if (light === "Yellow"){
+    console.log("Caution!");
+} else {
+    console.log("Stop");
+}
 
+//Second Problem//
 let kristyIntolerant = true;
 let baoBaoIntolerant = true;
 if(kristyIntolerant === true || 
@@ -12,3 +21,13 @@ console.log("Get Both Milks");
     } else {
     console.log("Just Regular Milk");
     }
+
+//Third Problem//
+let temp = 67;
+if (temp < 68) {
+    console.log("Too Cold");
+} else if (temp >= 68 && temp <= 72){
+    console.log("Perfect");
+} else if (temp > 72 && temp <= 76){
+    console.log("Is Warm");
+}
